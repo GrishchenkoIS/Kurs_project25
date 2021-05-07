70dc69bc1e45a09a591d4232014a166a0ce61755
Malomozhnova25 <valia.malomojnova@yandex.ru>
2021-05-08 00:20:46 +0500

    Добавила интерфейс и абстрактный класс

---
+++ src/main/java/kurs25/Calculator.java
@@ -0,0 +1,16 @@
+package kurs25;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public abstract  class Calculator implements  Check  {
+    double rate;
+    double sum;
+    int countMonth;
+    double pay;
+
+   
+    public abstract Double Calculator();
+
+}
---
+++ src/main/java/kurs25/Check.java
@@ -0,0 +1,7 @@
+package kurs25;
+
+public interface Check{
+    public boolean IsDouble(String d);
+    public boolean isInteger(String d);
+    public String Strahovka();
+}
