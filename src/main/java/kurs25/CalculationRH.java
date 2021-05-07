cdbad7607ad6a52d73b03312004df669ce3c9189
Malomozhnova25 <valia.malomojnova@yandex.ru>
2021-05-08 00:25:25 +0500

    Добавила классы расчетов

---
+++ src/main/java/kurs25/CalculationRH.java
@@ -0,0 +1,53 @@
+package kurs25;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+public class CalculationRH extends Calculator {
+	double res;
+	public CalculationRH() {
+		
+		res = FlodPoint(Calculator());
+	}
+	public static double FlodPoint(double value) {
+		value = Math.round(value * 100);
+		value = value/100;
+		return value;
+	}
+	
+	@Override
+	public Double Calculator() {
+		double sum = Calc.RequestCalc.first_result - Calc.RequestCalc.second_result;
+		rate = 8.3;
+		if (Strahovka().equals("Exist")) {
+			rate = rate - 1;
+		}
+		rate = rate/12/100/12;
+		if (Calc.RequestCalc.data_result == 0 || sum == 0) {
+			return (double) 0;
+		}else {
+		pay = rate * Math.pow(1+rate, Calc.RequestCalc.data_result)/(Math.pow(1+rate, Calc.RequestCalc.data_result)-1)*(Calc.RequestCalc.first_result+Calc.RequestCalc.second_result);
+		}
+		return pay;
+	}
+	@Override
+	public boolean IsDouble(String d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInteger(String d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String Strahovka() {
+		String info = (String) Calc.RequestCalc.strahovka;
+		return info;
+	};
+
+
+	
+
+	
+
+}
