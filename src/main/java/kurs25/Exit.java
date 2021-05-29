b7006a3e6ee58be18423cd84975668156a06ff15
mochalina <moch_alina@mail.ru>
2021-05-29 19:31:36 +0500

    исправила выход из учетной записи

--- src/main/java/kurs25/Exit.java
+++ src/main/java/kurs25/Exit.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name="Exit", urlPatterns="/JavaExit")
 public class Exit extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/index.jsp").forward(request, response);
+		Admin.status = 2;
+		request.getRequestDispatcher("/author.jsp").forward(request, response);
 	}
 	}
