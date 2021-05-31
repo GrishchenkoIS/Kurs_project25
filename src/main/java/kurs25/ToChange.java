ae66725cc55db3a5305dfc94d3cd1bf410308f73
GrishchenkoIS <Sherkios@mail.ru>
2021-05-31 10:22:43 +0500

    Изменил для проверки

--- src/main/java/kurs25/ToChange.java
+++ src/main/java/kurs25/ToChange.java
@@ -37,21 +37,9 @@ public class ToChange extends HttpServlet  {
 		}
 		
 		if ( Admin.status == 1 ) {
-			request.setAttribute("incorrect", "");
-        	request.setAttribute("changes", "<label for=\"changes\" class=\"changes__text\"> Изменить ставку :</label>\n"
-        			+ "                <input type=\"submit\" name=\"sign\" value=\"Изменить\" class=\"changes__submit input\">");
-        	request.setAttribute("display", "none");
-        	request.setAttribute("admin", "<div class=\"header__form\">\n"
-        			+ "                <label for=\"\" class=\"header__text\"> Здравствуйте администратор</label>\n"
-        			+ "                <input type=\"submit\" name=\"sign\" value=\"Выйти\" class=\"header__input input\"></div>");
-        	
+			
 		} else if (Admin.status == 0) {
-			request.setAttribute("incorrect", "");
-        	request.setAttribute("display", "none");
-        	request.setAttribute("admin", "<div class=\"header__form\">\n"
-        			+ "                <label for=\"\" class=\"header__text\"> Здравсвуйте пользователь</label>\n"
-        			+ "                <input type=\"submit\" name=\"sign\" value=\"Выйти\" class=\"header__input input\"></div>");
-        	
+			
 		}
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
